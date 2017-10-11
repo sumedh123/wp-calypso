fa8b5f73517cc52201bbc4233f0185f501653564
Andrés Maneiro <andres.maneiro@automattic.com>
2017-10-11 13:54:03 +0200

    State: extract actions related to happychat.chat

--- client/state/happychat/actions.js
+++ client/state/happychat/actions.js
@@ -3,19 +3,7 @@
 /**
  * Internal dependencies
  */
-import {
-	HAPPYCHAT_BLUR,
-	HAPPYCHAT_FOCUS,
-	HAPPYCHAT_SET_CHAT_STATUS,
-	HAPPYCHAT_SET_MESSAGE,
-} from 'state/action-types';
-
-export const setHappychatChatStatus = status => ( {
-	type: HAPPYCHAT_SET_CHAT_STATUS,
-	status,
-} );
+import { HAPPYCHAT_BLUR, HAPPYCHAT_FOCUS } from 'state/action-types';
 
 export const blur = () => ( { type: HAPPYCHAT_BLUR } );
 export const focus = () => ( { type: HAPPYCHAT_FOCUS } );
-
-export const setChatMessage = message => ( { type: HAPPYCHAT_SET_MESSAGE, message } );
